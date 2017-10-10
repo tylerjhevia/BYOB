675a9e8933aa5a81f4b3b8615a29aa7b651c9c33
Tyler Hevia <tylerhevia@gmail.com>
2017-10-10 15:18:11 -0600

    seed data for beers and breweries table

---
+++ db/seeds/dev/byob.js
@@ -0,0 +1,51 @@
+const beers = require("../../../co-beer-data");
+const breweries = require("../../../co-breweries-data");
+
+exports.seed = (knex, Promise) => {
+  return knex("beers")
+    .del()
+    .then(() => knex("breweries").del())
+    .then(() => {
+      let breweryPromises = [];
+
+      breweries.forEach(brewery =>
+        breweryPromises.push(createBrewery(knex, brewery))
+      );
+      return Promise.all(breweryPromises);
+    })
+    .then(breweryID => {
+      let breweryPromises = [];
+
+      beers.forEach(beer => {
+        breweryPromises.push(
+          createBeer(knex, {
+            name: beer["beerName"],
+            brewery: beer["breweryName"],
+            type: beer["beerType"],
+            breweryID: knex
+              .select("id")
+              .from("breweries")
+              .where("name", beer["breweryName"])
+          })
+        );
+      });
+      return Promise.all(breweryPromises);
+    })
+    .then(() => {
+      console.log("Seeding is complete.");
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
+
+const createBrewery = (knex, brewery) => {
+  return knex("breweries").insert({
+    name: brewery.name,
+    location: brewery.location,
+    beerCount: parseInt(brewery.numOfBeers),
+    year: parseInt(brewery.yearFounded)
+  });
+};
+
+const createBeer = (knex, beer) => {
+  return knex("beers").insert(beer);
+};
