612b3f89a1f12299bfca15311198c94e60bfd9e8
David Becker <david@beckersite.com>
2017-10-10 15:39:22 -0600

    Make brewery test data file

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,26 @@ module.exports = {
       directory: "./db/seeds/dev"
     },
     useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/byob_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true,
+    seeds: {
+      directory: './db/seeds/test'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
---
+++ test-brewery-data.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    "location": "Castle Rock",
+    "name": "105 West Brewing Company",
+    "numOfBeers": "20",
+    "yearFounded": "2015"
+  },
+  {
+    "location": "Louisville",
+    "name": "12 Degree Brewing",
+    "numOfBeers": "54",
+    "yearFounded": "2013"
+  },
+]
