72bfbdbb82d5c6f61f043ac4d4869dededf598a3
David Becker <david@beckersite.com>
2017-10-10 16:07:42 -0600

    Add get request for all breweries

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'BYOB';
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+
+app.get('/api/v1/breweries', (request, response) => {
+  database('breweries')
+    .select()
+    .then((breweries) => {
+      response.status(200).json(breweries);
+    })
+    .catch((error) => {
+      response.sendStatus(500).json({ error });
+    });
+});
+
+module.exports = app;
